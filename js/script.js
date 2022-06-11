8525bc0efb2787c47dad7e1cc413d99827131644
Kolawole Adeyemo <kolawole_adeyemo@yahoo.com>
2022-06-11 21:13:02 +0100

    add modu

--- js/script.js
+++ js/script.js
@@ -7,3 +7,37 @@ let number2 = parseInt(prompt("Input your number"));
 let number3 = parseInt(prompt("Input your number"));
 
 // Addtion of the three numbers
+
+let add = ( number1 + number2 + number3);
+
+// modulos of the three numbers
+
+let modu = add % 2
+
+// if statement
+
+if (modu ===0) {
+    alert("The sum of the three numbers is EVEN");
+}
+
+else{
+    alert("The sum of the three numbers is ODD")
+}
+
+// function Addtion(){
+//     let cal= (number1 + number2 + number3) % 2
+
+//     return cal
+// }
+
+// if (cal ===0) { alert ("The sum of the three numbers is EVEN")
+
+// }
+
+// else {
+//    alert ("The sum of the three is ODD")
+// }
+
+// alert(Addtion());
+
+// document.write(Addtion());
