460cd2889cc08464edbdf06cf8163778944bae70
Kolawole Adeyemo <kolawole_adeyemo@yahoo.com>
2022-06-11 19:49:38 +0100

    add number

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// As a users to input there value
+
+let number1 = parseInt(prompt("Input your number"));
+
+let number2 = parseInt(prompt("Input your number"));
+
+let number3 = parseInt(prompt("Input your number"));
+
+// Addtion of the three numbers
